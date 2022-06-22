abf2646cea8a020cf496d643bd2fcf1e3506da58
loubard <yannick.cardini@gmx.com>
2022-06-22 13:38:03 +0700

    add cors :bustop:

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express()
+
+var corsOptions = {
+    origin: 'https://www.mainecoon.click',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions))
 
 app.use(logger('dev'));
 app.use(express.json());
