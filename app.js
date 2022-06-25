6bc6388a7e1feb22964d349d13f0f2d9d24ab087
loubard <yannick.cardini@gmx.com>
2022-06-25 12:44:38 +0700

    fix bug :bug:

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express()
-app.listen(3000, 'localhost', function() {
+app.listen(3001, 'localhost', function() {
   console.log("... port %d in %s mode", app.address().port, app.settings.env);
 });
 
